651f965a90d3af27ed47e9a9c4e223a5c076e685
Anshul Agrawal <anshulagrawal134@gmail.com>
2019-06-07 15:41:32 -0700

    package.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const hbs = require('express-handlebars');
 var NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
 const path = require('path');
-var server=require('https').createServer(app);
+var server=require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
 //USING .env file for enviroment variables
